92f79a7c06982a919ab4f2d787a1e226be2fd5e1
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-01-31 14:20:30 -0300

    feat: safe area layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,20 +4,19 @@ import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
+export { ErrorBoundary } from 'expo-router';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const insets = useSafeAreaInsets();
+
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
@@ -38,5 +37,13 @@ export default function RootLayout() {
     return null;
   }
 
-  return <Routes />;
+  return (
+    <SafeAreaProvider
+      style={{
+        paddingTop: insets.top,
+      }}
+    >
+      <Routes />
+    </SafeAreaProvider>
+  );
 }
