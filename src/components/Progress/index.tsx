7b295812014d53299b8c1791fca1373dffabb80e
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-07 12:17:36 -0300

    feat: progress animation

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { getHours, getMinutes, getSeconds } from 'date-fns';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { useTheme } from 'styled-components';
 
 import { Clock } from './styles';
 
 const Progress = () => {
-  const fill = 60;
-  const clock = '12:00';
   const theme = useTheme();
+
+  const date = new Date();
+
+  const [seconds, setSeconds] = useState(getSeconds(date));
+
+  const hours = getHours(date);
+  const minutes = getMinutes(date);
+
+  const clock = `${hours}:${minutes}`;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (seconds === 60) {
+      setSeconds(0);
+    }
+  }, [seconds]);
+
+  const fill = (seconds / 60) * 100;
+
   return (
     <AnimatedCircularProgress
       size={220}
@@ -16,6 +41,7 @@ const Progress = () => {
       fill={fill}
       tintColor={theme.colors.primary[600]}
       backgroundColor={theme.colors.primary[200]}
+      rotation={0}
     >
       {() => <Clock>{clock}</Clock>}
     </AnimatedCircularProgress>
